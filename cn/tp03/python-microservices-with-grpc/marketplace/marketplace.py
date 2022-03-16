5d2ecad94de41aeca48b0629d550458d62558c84
Tomás Dias <tomasalexdias@gmail.com>
2022-03-16 03:19:08 +0000

    Update docker files from tp03

--- cn/tp03/python-microservices-with-grpc/marketplace/marketplace.py
+++ cn/tp03/python-microservices-with-grpc/marketplace/marketplace.py
@@ -19,7 +19,11 @@ recommendations_host = os.getenv("RECOMMENDATIONS_HOST", "localhost")
 # recommendations_channel = grpc.secure_channel(
 #     f"{recommendations_host}:443", creds
 # )
-recommendations_channel = grpc.insecure_channel(f"{recommendations_host}:50051")
+# recommendations_channel = grpc.insecure_channel(f"{recommendations_host}:50051")
+recommendations_host = os.getenv("RECOMMENDATIONS_HOST", "localhost")
+recommendations_channel = grpc.insecure_channel(
+ f"{recommendations_host}:50051"
+)
 recommendations_client = RecommendationsStub(recommendations_channel)
 
 
