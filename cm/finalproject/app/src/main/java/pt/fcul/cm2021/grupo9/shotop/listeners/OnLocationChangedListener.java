e94a12b0e922f9c9bea3bafa7a3cb65c1a4c2651
Tomás Dias <tomasalexdias@gmail.com>
2022-06-21 22:31:59 +0100

    :art: Improve repository structure

--- cm/finalproject/app/src/main/java/pt/fcul/cm2021/grupo9/shotop/listeners/OnLocationChangedListener.java
+++
@@ -1,7 +0,0 @@
-package pt.fcul.cm2021.grupo9.shotop.listeners;
-
-import com.google.android.gms.location.LocationResult;
-
-public interface OnLocationChangedListener {
-    public void onLocationChanged(LocationResult locationResult);
-}
